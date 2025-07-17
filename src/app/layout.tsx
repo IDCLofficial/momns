c1c3bdb042a94b7e2c92d0baf06d2f63c47ef543
Techxie-bc <donnaogbu@gmail.com>
2025-07-17 10:03:47 +0100

    Nextjs setup is ready

---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: " Ministry of Industry, Mines, and Solid Minerals",
+  description: "The Ministry of Industry, Mines, and Solid Minerals in Imo State is responsible for overseeing and developing the state's industrial sector, as well as managing its mineral resources.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
