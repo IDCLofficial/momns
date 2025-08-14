0c9f3dac18cc72783030f6c322432650aa771bae
Techxie-bc <donnaogbu@gmail.com>
2025-08-14 14:55:41 +0100

    contentful for project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
+export const dynamic = 'force-dynamic'
+
 import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
-
-
+import GlobalLoader from '@/components/GlobalLoader'
 
 export const metadata: Metadata = {
   title: " Ministry of Industry, Mines, and Solid Minerals",
@@ -17,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <GlobalLoader/>
         <Navbar/>
         {children}
       </body>
